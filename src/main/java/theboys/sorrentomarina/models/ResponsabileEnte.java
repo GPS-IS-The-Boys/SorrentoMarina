e9b7f548cef6b6d49ccfeaf202d340940cf4a184
Daniele <danielecarangelo99@gmail.com>
2020-12-28 10:52:44 +0100

    add ResponsabileEnte Test

--- src/main/java/theboys/sorrentomarina/models/ResponsabileEnte.java
+++ src/main/java/theboys/sorrentomarina/models/ResponsabileEnte.java
@@ -9,27 +9,27 @@ import lombok.ToString;
 @ToString
 public class ResponsabileEnte {
 
-    private String nome, cognome, email, username, password;
-    private int id;
+  private String nome, cognome, email, username, password;
+  private int id;
 
-    public  ResponsabileEnte(){
-        super();
-    }
+  public ResponsabileEnte() {
+    super();
+  }
 
-    public ResponsabileEnte(int id, String nome, String cognome, String email, String username, String password){
-        this.id=id;
-        this.nome=nome;
-        this.cognome=cognome;
-        this.email=email;
-        this.username=username;
-        this.password=password;
-    }
+  public ResponsabileEnte(int id, String nome, String cognome, String email, String username, String password) {
+    this.id = id;
+    this.nome = nome;
+    this.cognome = cognome;
+    this.email = email;
+    this.username = username;
+    this.password = password;
+  }
 
-    public ResponsabileEnte(String nome, String cognome, String email, String username, String password){
-        this.nome=nome;
-        this.cognome=cognome;
-        this.email=email;
-        this.username=username;
-        this.password=password;
-    }
+  public ResponsabileEnte(String nome, String cognome, String email, String username, String password) {
+    this.nome = nome;
+    this.cognome = cognome;
+    this.email = email;
+    this.username = username;
+    this.password = password;
+  }
 }
