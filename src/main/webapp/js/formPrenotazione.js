7891fbf81a796c1788bde108fe49928586631661
Daniele <danielecarangelo99@gmail.com>
2021-01-07 15:44:57 +0100

    Correct bug griglia prenotazione

--- src/main/webapp/js/formPrenotazione.js
+++ src/main/webapp/js/formPrenotazione.js
@@ -1,45 +1,52 @@
-function nextPrenotazione2(){
+function nextPrenotazione2() {
 
-    $('#prenotazione1').slideToggle(500);
-    $('#prenotazione2').slideToggle(500);
-    var div = document.getElementById("step2");
-    div.classList.add("active");
+
     var numPosti = $('#numPosti option:selected').val();
 
     var date = new Date($("#dataInizio").val());
     var day = date.getDate();
     var month = date.getMonth() + 1;
     var year = date.getFullYear();
-    var dataInizio=([year, month, day].join('-'));
+    var dataInizio = ([year, month, day].join('-'));
 
     var fine = new Date($("#dataFine").val());
     var dayFine = fine.getDate();
     var monthFine = fine.getMonth() + 1;
     var yearFine = fine.getFullYear();
-    var dataFine =([yearFine, monthFine, dayFine].join('-'));
-    var lista="";
-
-    $.post("postiOccupati?dataInizio="+ dataInizio +"&dataFine="+dataFine, function(data, status){
-       //alert(data.occupati + " status:" +status);
-        lista = data.occupati.join(",");
-        console.log(lista);
-        sessionStorage.setItem("postiOccupati",lista);
-    });
+    var dataFine = ([yearFine, monthFine, dayFine].join('-'));
+    var lista = "";
 
 
-   // sessionStorage.clear();
-    sessionStorage.setItem("dataInizio", dataInizio);
-    sessionStorage.setItem("dataFine",dataFine);
-    sessionStorage.setItem("numPosti", numPosti);
+    if (numPosti == 0 || isNaN(date)  || isNaN(fine)) {
+        alert("Compila tutti i campi");
+    }
+    else {
+        $('#prenotazione1').slideToggle(500);
+        $('#prenotazione2').slideToggle(500);
+        var div = document.getElementById("step2");
+        div.classList.add("active");
 
-    jQuery.ajax({
-        url: "/js/griglia.js",
-        dataType: "script",
-        async: true,
-    });
+        $.post("postiOccupati?dataInizio=" + dataInizio + "&dataFine=" + dataFine, function (data) {
+            //alert(data.occupati + " status:" +status);
+            lista = data.occupati.join(",");
+            console.log(lista);
+            sessionStorage.setItem("postiOccupati", lista);
+        });
 
+
+        // sessionStorage.clear();
+        sessionStorage.setItem("dataInizio", dataInizio);
+        sessionStorage.setItem("dataFine", dataFine);
+        sessionStorage.setItem("numPosti", numPosti);
+
+        jQuery.ajax({
+            url: "/js/griglia.js",
+            dataType: "script",
+            async: true,
+        });
+
+    }
 }
-
 function selectChange(){
 
     jQuery.ajax({
--- src/main/webapp/js/griglia.js
+++ src/main/webapp/js/griglia.js
@@ -5,7 +5,7 @@ var firstSeatLabel = 1;
 
 
 
-
+		var sc;
 
         var righe = parseInt(document.getElementById("1").getAttribute("value"));
 		var colonne = parseInt(document.getElementById("2").getAttribute("value"));
@@ -31,22 +31,34 @@ var firstSeatLabel = 1;
 
 		$('.front-indicator').css('width',j*30);
 
-	console.log(numPosti);
-
 	function  totalPersone(){
 		totalePosti=0;
 		totalePosti = numPosti*prezzoSingolo;
 		return totalePosti;
 	}
 
-	var posti = "";
+	var posti = [];
 
-	posti =	sessionStorage.getItem("postiOccupati");
+
+
+$(document).ajaxComplete(function () {
+	posti = sessionStorage.getItem("postiOccupati");
+	if (posti != null && posti != undefined && posti.length >= 1) {
+
 	posti = posti.split(',');
-//$(document).ajaxComplete(function (){
+	for(var i in posti){
+		console.log(posti[i]);
+	}
+	console.log("create grid");
+	createGrid();
 
+}
+	else {
+	console.log("clear grid");
+	clearGrid();
+	}
 
-//});
+});
 
 
 
@@ -60,75 +72,75 @@ $(document).ready(function() {
 	});
 	var $cart = $('#selected-seats'),
 		$counter = $('#counter'),
-		$total = $('#total'),
-		sc = $('#seat-map').seatCharts({
-			map:seatMap,
-			seats: {
-				f: {
-					price   : 10,
-					classes : 'first-class', //your custom CSS class
-					category: 'Ombrellone'
-				},
-				e: {
-					price   : 40,
-					classes : 'economy-class', //your custom CSS class
-					category: 'Economy Class'
-				}
-
+		$total = $('#total');
+	sc = $('#seat-map').seatCharts({
+		map: seatMap,
+		seats: {
+			f: {
+				price: 10,
+				classes: 'first-class', //your custom CSS class
+				category: 'Ombrellone'
 			},
-			naming : {
-				top : false,
-				getLabel : function (character, row, column) {
-					return firstSeatLabel++;
-				},
-			},
-			legend : {
-				node : $('#legend'),
-				items : [
-					[ 'f', 'available',   'Disponibili' ],
-					[ 'e', 'available',   'Selezionato'],
-					[ 'f', 'unavailable', 'Non disponibile']
-				]
-			},
-			click: function () {
-
-				if (this.status() == 'available') {
-					//let's create a new <li> which we'll add to the cart items
-					$('<li>'+this.data().category+' Num: # '+this.settings.label+'<a href="#" class="cancel-cart-item">[cancel]</a></li>')
-						.attr('id', 'cart-item-'+this.settings.id)
-						.data('seatId', this.settings.id)
-						.appendTo($cart);
-
-					/*
-                     * Lets update the counter and total
-                     *
-                     * .find function will not find the current seat, because it will change its stauts only after return
-                     * 'selected'. This is why we have to add 1 to the length and the current seat price to the total.
-                     */
-					console.log(sc.find('selected'));
-					$counter.text(sc.find('selected').length+1);
-					//$total.text(recalculateTotal(sc)+this.data().price);
-
-					return 'selected';
-				} else if (this.status() == 'selected') {
-					//update the counter
-					$counter.text(sc.find('selected').length-1);
-					//and total
-					//$total.text(recalculateTotal(sc)-this.data().price);
-
-					//remove the item from our cart
-					$('#cart-item-'+this.settings.id).remove();
-
-					//seat has been vacated
-					return 'available';
-				} else if (this.status() == 'unavailable') {
-					//seat has been already booked
-					return 'unavailable';
-				} else {
-					return this.style();
-				}
+			e: {
+				price: 40,
+				classes: 'economy-class', //your custom CSS class
+				category: 'Economy Class'
 			}
-		});
+
+		},
+		naming: {
+			top: false,
+			getLabel: function (character, row, column) {
+				return firstSeatLabel++;
+			},
+		},
+		legend: {
+			node: $('#legend'),
+			items: [
+				['f', 'available', 'Disponibili'],
+				['e', 'available', 'Selezionato'],
+				['f', 'unavailable', 'Non disponibile']
+			]
+		},
+		click: function () {
+
+			if (this.status() == 'available') {
+				//let's create a new <li> which we'll add to the cart items
+				$('<li>' + this.data().category + ' Num: # ' + this.settings.label + '<a href="#" class="cancel-cart-item">[cancel]</a></li>')
+					.attr('id', 'cart-item-' + this.settings.id)
+					.data('seatId', this.settings.id)
+					.appendTo($cart);
+
+				/*
+                 * Lets update the counter and total
+                 *
+                 * .find function will not find the current seat, because it will change its stauts only after return
+                 * 'selected'. This is why we have to add 1 to the length and the current seat price to the total.
+                 */
+				console.log(sc.find('selected'));
+				$counter.text(sc.find('selected').length + 1);
+				//$total.text(recalculateTotal(sc)+this.data().price);
+
+				return 'selected';
+			} else if (this.status() == 'selected') {
+				//update the counter
+				$counter.text(sc.find('selected').length - 1);
+				//and total
+				//$total.text(recalculateTotal(sc)-this.data().price);
+
+				//remove the item from our cart
+				$('#cart-item-' + this.settings.id).remove();
+
+				//seat has been vacated
+				return 'available';
+			} else if (this.status() == 'unavailable') {
+				//seat has been already booked
+				return 'unavailable';
+			} else {
+				return this.style();
+			}
+		}
+	});
 	recalculateTotal(sc);
 	//this will handle "[cancel]" link clicks
 	$('#selected-seats').on('click', '.cancel-cart-item', function () {
@@ -139,13 +151,11 @@ $(document).ready(function() {
 	//let's pretend some seats have already been booked
 	//sc.get(['1_2', '4_1', '6_1', '6_2']).status('unavailable');
 
-	for(var i in posti){
-		sc.get(posti[i]).status('unavailable');
-	}
-	console.log(posti[0]);
 	$total.text(totalPersone());
 
 });
+
+
 
 
 function recalculateTotal(sc) {
@@ -160,7 +170,27 @@ function recalculateTotal(sc) {
 	return total;
 }
 
+function clearGrid() {
+	if (sc != null) {
+	sc.find('unavailable').status('available');
+	console.log('clear ok');
+	}
+}
+
+function createGrid(){
+	var i;
+	if( sc != null) {
+		for (i in posti) {
+				sc.get(posti[i]).status('unavailable');
+				console.log(posti[i]);
+
+		}
+
+	}
+}
+
 var selezionabili = Math.ceil(numPosti/3);
+
 
 
 })(jQuery);
