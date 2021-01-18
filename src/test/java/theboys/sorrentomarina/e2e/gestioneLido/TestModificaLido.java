a2eb663683a56e3592830f0ab7f6dc237e327045
Daniele <danielecarangelo99@gmail.com>
2021-01-18 23:10:06 +0100

    Add TestSystem gestione utente e gestioneLido

---
+++ src/test/java/theboys/sorrentomarina/e2e/gestioneLido/TestModificaLido.java
@@ -0,0 +1,114 @@
+package theboys.sorrentomarina.e2e.gestioneLido;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import theboys.sorrentomarina.e2e.SystemTestCase;
+
+public class TestModificaLido extends SystemTestCase {
+
+  private static WebDriver driver;
+
+  @Before
+  public void setup(){
+    driver = SystemTestCase.driver;
+  }
+
+  @Test
+  public void testModificaOk(){
+    driver.get("http://localhost:8080/SorrentoMarina/login");
+    WebElement userField = driver.findElement(By.name("username"));
+    userField.sendKeys("alfio");
+    WebElement passwordField = driver.findElement(By.name("password"));
+    passwordField.sendKeys("Forzajuve1");
+    WebElement form = driver.findElement(By.name("LoginForm"));
+    form.submit();
+    driver.get("http://localhost:8080/SorrentoMarina/mostraModificaLido");
+    WebElement addressField = driver.findElement(By.name("indirizzoLido"));
+    addressField.clear();
+    addressField.sendKeys("Via Logoreti");
+    WebElement formModifica = driver.findElement(By.name("modificaLido"));
+    formModifica.submit();
+    String url = driver.getCurrentUrl();
+    Assert.assertEquals(url,"http://localhost:8080/SorrentoMarina/dashboard");
+  }
+
+  @Test
+  public void testModificaNomeNotOk(){
+    driver.get("http://localhost:8080/SorrentoMarina/login");
+    WebElement userField = driver.findElement(By.name("username"));
+    userField.sendKeys("alfio");
+    WebElement passwordField = driver.findElement(By.name("password"));
+    passwordField.sendKeys("Forzajuve1");
+    WebElement form = driver.findElement(By.name("LoginForm"));
+    form.submit();
+    driver.get("http://localhost:8080/SorrentoMarina/mostraModificaLido");
+    WebElement nomeField = driver.findElement(By.name("nomeLido"));
+    nomeField.clear();
+    nomeField.sendKeys("   ");
+    WebElement formModifica = driver.findElement(By.name("modificaLido"));
+    formModifica.submit();
+    Boolean isModificaOk = driver.getPageSource().contains("Nome del Lido non valido");
+    Assert.assertTrue(isModificaOk);
+  }
+
+  @Test
+  public void testModificaIndirizzoNotOk(){
+    driver.get("http://localhost:8080/SorrentoMarina/login");
+    WebElement userField = driver.findElement(By.name("username"));
+    userField.sendKeys("alfio");
+    WebElement passwordField = driver.findElement(By.name("password"));
+    passwordField.sendKeys("Forzajuve1");
+    WebElement form = driver.findElement(By.name("LoginForm"));
+    form.submit();
+    driver.get("http://localhost:8080/SorrentoMarina/mostraModificaLido");
+    WebElement indirizzoField = driver.findElement(By.name("indirizzoLido"));
+    indirizzoField.clear();
+    indirizzoField.sendKeys("   ");
+    WebElement formModifica = driver.findElement(By.name("modificaLido"));
+    formModifica.submit();
+    Boolean isModificaOk = driver.getPageSource().contains("Indirizzo del Lido non valido");
+    Assert.assertTrue(isModificaOk);
+  }
+
+  @Test
+  public void testModificaTelNotOk(){
+    driver.get("http://localhost:8080/SorrentoMarina/login");
+    WebElement userField = driver.findElement(By.name("username"));
+    userField.sendKeys("alfio");
+    WebElement passwordField = driver.findElement(By.name("password"));
+    passwordField.sendKeys("Forzajuve1");
+    WebElement form = driver.findElement(By.name("LoginForm"));
+    form.submit();
+    driver.get("http://localhost:8080/SorrentoMarina/mostraModificaLido");
+    WebElement telefonoField = driver.findElement(By.name("telefonoLido"));
+    telefonoField.clear();
+    telefonoField.sendKeys("123");
+    WebElement formModifica = driver.findElement(By.name("modificaLido"));
+    formModifica.submit();
+    Boolean isModificaOk = driver.getPageSource().contains("Telefono del Lido non valido");
+    Assert.assertTrue(isModificaOk);
+  }
+
+  @Test
+  public void testModificaPathNotOk(){
+    driver.get("http://localhost:8080/SorrentoMarina/login");
+    WebElement userField = driver.findElement(By.name("username"));
+    userField.sendKeys("alfio");
+    WebElement passwordField = driver.findElement(By.name("password"));
+    passwordField.sendKeys("Forzajuve1");
+    WebElement form = driver.findElement(By.name("LoginForm"));
+    form.submit();
+    driver.get("http://localhost:8080/SorrentoMarina/mostraModificaLido");
+    WebElement pathField = driver.findElement(By.name("logoLido"));
+    pathField.clear();
+    pathField.sendKeys("123");
+    WebElement formModifica = driver.findElement(By.name("modificaLido"));
+    formModifica.submit();
+    Boolean isModificaOk = driver.getPageSource().contains("Path logo del Lido non valido");
+    Assert.assertTrue(isModificaOk);
+  }
+}
---
+++ src/test/java/theboys/sorrentomarina/e2e/gestioneUtente/TestRegistrazioneTurista.java
@@ -0,0 +1,145 @@
+package theboys.sorrentomarina.e2e.gestioneUtente;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import theboys.sorrentomarina.e2e.SystemTestCase;
+
+public class TestRegistrazioneTurista extends SystemTestCase {
+
+  private static WebDriver driver;
+
+  @Before
+  public void setup(){
+    driver = SystemTestCase.driver;
+  }
+
+  @Test
+  public void testRegistrazioneOk(){
+    driver.get("http://localhost:8080/SorrentoMarina/registrazione");
+    WebElement nomeField = driver.findElement(By.name("nome"));
+    nomeField.sendKeys("Matteo");
+    WebElement cognomeField = driver.findElement(By.name("cognome"));
+    cognomeField.sendKeys("Mattei");
+    WebElement emailField = driver.findElement(By.name("email"));
+    emailField.sendKeys("Matteomattei@gmail.com");
+    WebElement usernameField = driver.findElement(By.name("username"));
+    usernameField.sendKeys("matteomattei");
+    WebElement passwordField = driver.findElement(By.name("password"));
+    passwordField.sendKeys("Matteomattei1");
+    WebElement confermaField = driver.findElement(By.name("passwordConferma"));
+    confermaField.sendKeys("Matteomattei1");
+    WebElement form = driver.findElement(By.name("registrazioneForm"));
+    form.submit();
+    Boolean isRegistrazioneOk = driver.getPageSource().contains("Benvenuto " + "matteomattei");
+    Assert.assertTrue(isRegistrazioneOk);
+  }
+
+  @Test
+  public void testRegistrazioneUsernameNotOk(){
+    driver.get("http://localhost:8080/SorrentoMarina/registrazione");
+    WebElement nomeField = driver.findElement(By.name("nome"));
+    nomeField.sendKeys("Matteo");
+    WebElement cognomeField = driver.findElement(By.name("cognome"));
+    cognomeField.sendKeys("Mattei");
+    WebElement emailField = driver.findElement(By.name("email"));
+    emailField.sendKeys("Matteomattei@gmail.com");
+    WebElement usernameField = driver.findElement(By.name("username"));
+    usernameField.sendKeys("matt");
+    WebElement passwordField = driver.findElement(By.name("password"));
+    passwordField.sendKeys("Matteomattei1");
+    WebElement confermaField = driver.findElement(By.name("passwordConferma"));
+    confermaField.sendKeys("Matteomattei1");
+    WebElement form = driver.findElement(By.name("registrazioneForm"));
+    form.submit();
+    Boolean isRegistrazioneOk = driver.getPageSource().contains("Campo Username non valido");
+    Assert.assertTrue(isRegistrazioneOk);
+  }
+
+  @Test
+  public void testRegistrazionePasswrdNotOk(){
+    driver.get("http://localhost:8080/SorrentoMarina/registrazione");
+    WebElement nomeField = driver.findElement(By.name("nome"));
+    nomeField.sendKeys("Matteo");
+    WebElement cognomeField = driver.findElement(By.name("cognome"));
+    cognomeField.sendKeys("Mattei");
+    WebElement emailField = driver.findElement(By.name("email"));
+    emailField.sendKeys("Matteomattei@gmail.com");
+    WebElement usernameField = driver.findElement(By.name("username"));
+    usernameField.sendKeys("matteomattei");
+    WebElement passwordField = driver.findElement(By.name("password"));
+    passwordField.sendKeys("matt");
+    WebElement confermaField = driver.findElement(By.name("passwordConferma"));
+    confermaField.sendKeys("matt");
+    WebElement form = driver.findElement(By.name("registrazioneForm"));
+    form.submit();
+    Boolean isRegistrazioneOk = driver.getPageSource().contains("Campo Password non valido");
+    Assert.assertTrue(isRegistrazioneOk);
+  }
+
+  @Test
+  public void testRegistrazionePasswrdNotEquals(){
+    driver.get("http://localhost:8080/SorrentoMarina/registrazione");
+    WebElement nomeField = driver.findElement(By.name("nome"));
+    nomeField.sendKeys("Matteo");
+    WebElement cognomeField = driver.findElement(By.name("cognome"));
+    cognomeField.sendKeys("Mattei");
+    WebElement emailField = driver.findElement(By.name("email"));
+    emailField.sendKeys("Matteomattei@gmail.com");
+    WebElement usernameField = driver.findElement(By.name("username"));
+    usernameField.sendKeys("matteomattei");
+    WebElement passwordField = driver.findElement(By.name("password"));
+    passwordField.sendKeys("Matteomattei1");
+    WebElement confermaField = driver.findElement(By.name("passwordConferma"));
+    confermaField.sendKeys("Matteomattei2");
+    WebElement form = driver.findElement(By.name("registrazioneForm"));
+    form.submit();
+    Boolean isRegistrazioneOk = driver.getPageSource().contains("Password e Conferma non coincidono");
+    Assert.assertTrue(isRegistrazioneOk);
+  }
+
+  @Test
+  public void testRegistrazioneNameNotOk(){
+    driver.get("http://localhost:8080/SorrentoMarina/registrazione");
+    WebElement nomeField = driver.findElement(By.name("nome"));
+    nomeField.sendKeys("00000");
+    WebElement cognomeField = driver.findElement(By.name("cognome"));
+    cognomeField.sendKeys("Mattei");
+    WebElement emailField = driver.findElement(By.name("email"));
+    emailField.sendKeys("Matteomattei@gmail.com");
+    WebElement usernameField = driver.findElement(By.name("username"));
+    usernameField.sendKeys("matteomattei");
+    WebElement passwordField = driver.findElement(By.name("password"));
+    passwordField.sendKeys("Matteomattei1");
+    WebElement confermaField = driver.findElement(By.name("passwordConferma"));
+    confermaField.sendKeys("Matteomattei1");
+    WebElement form = driver.findElement(By.name("registrazioneForm"));
+    form.submit();
+    Boolean isRegistrazioneOk = driver.getPageSource().contains("Campo Nome non valido");
+    Assert.assertTrue(isRegistrazioneOk);
+  }
+  @Test
+  public void testRegistrazioneMailNotOk(){
+    driver.get("http://localhost:8080/SorrentoMarina/registrazione");
+    WebElement nomeField = driver.findElement(By.name("nome"));
+    nomeField.sendKeys("matteo");
+    WebElement cognomeField = driver.findElement(By.name("cognome"));
+    cognomeField.sendKeys("Mattei");
+    WebElement emailField = driver.findElement(By.name("email"));
+    emailField.sendKeys("12345");
+    WebElement usernameField = driver.findElement(By.name("username"));
+    usernameField.sendKeys("matteomattei");
+    WebElement passwordField = driver.findElement(By.name("password"));
+    passwordField.sendKeys("Matteomattei1");
+    WebElement confermaField = driver.findElement(By.name("passwordConferma"));
+    confermaField.sendKeys("Matteomattei1");
+    WebElement form = driver.findElement(By.name("registrazioneForm"));
+    form.submit();
+    Boolean isRegistrazioneOk = driver.getPageSource().contains("Campo Email non valido");
+    Assert.assertTrue(isRegistrazioneOk);
+  }
+
+}
