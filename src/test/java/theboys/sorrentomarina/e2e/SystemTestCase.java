1b9ce231e5468f9bb7d80ff445ee73d7c64d5048
Daniele <danielecarangelo99@gmail.com>
2021-01-18 12:20:13 +0100

    Add TestSystem Login

--- src/test/java/theboys/sorrentomarina/e2e/SystemTestCase.java
+++ src/test/java/theboys/sorrentomarina/e2e/SystemTestCase.java
@@ -1,27 +1,41 @@
 package theboys.sorrentomarina.e2e;
 
+
+import io.github.bonigarcia.wdm.config.DriverManagerType;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import theboys.sorrentomarina.actions.ActionSetupDB;
+import theboys.sorrentomarina.utils.MockConnection;
+
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
 
 /**
  *@author theboys
  */
-public class SystemTestCase {
+public class SystemTestCase  {
 
   protected static WebDriver driver;
 
-  @BeforeAll
-  public static void setupClass() {
+  @BeforeClass
+  public static void setupClass() throws FileNotFoundException, SQLException {
     ChromeOptions chromeOptions = new ChromeOptions();
-    chromeOptions.addArguments("--headless"); // per non lanciare gui
+    //chromeOptions.addArguments("--headless"); // per non lanciare gui
+    ChromeDriverManager .getInstance(DriverManagerType.CHROME).setup();
     driver = new ChromeDriver(chromeOptions);
   }
 
+  /*
   @AfterAll
   public static void tearDown() {
     driver.close();
   }
+
+   */
 }
---
+++ src/test/java/theboys/sorrentomarina/e2e/gestioneUtente/TestLoginEnte.java
@@ -0,0 +1,76 @@
+package theboys.sorrentomarina.e2e.gestioneUtente;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import theboys.sorrentomarina.e2e.SystemTestCase;
+
+public class TestLoginEnte extends SystemTestCase {
+
+  private static WebDriver driver;
+
+  @Before
+  public void setup(){
+    driver = SystemTestCase.driver;
+  }
+
+  @Test
+  public void testLoginEnteOk(){
+    driver.get("http://localhost:8080/SorrentoMarina/login");
+    WebElement userField = driver.findElement(By.name("username"));
+    userField.sendKeys("root");
+    WebElement passwordField = driver.findElement(By.name("password"));
+    passwordField.sendKeys("Password1");
+    WebElement form = driver.findElement(By.name("LoginForm"));
+    form.submit();
+    driver.get("http://localhost:8080/SorrentoMarina/dashboardEnte");
+    Boolean isLoginOk = driver.getPageSource().contains("Dashboard del Responsabile Ente");
+    Assert.assertTrue(isLoginOk);
+
+  }
+
+  @Test
+  public void testLoginEnteUsernameNotOk(){
+    driver.get("http://localhost:8080/SorrentoMarina/login");
+    WebElement userField = driver.findElement(By.name("username"));
+    userField.sendKeys("123");
+    WebElement passwordField = driver.findElement(By.name("password"));
+    passwordField.sendKeys("456");
+    WebElement form = driver.findElement(By.name("LoginForm"));
+    form.submit();
+    Boolean isLoginNotOk = driver.getPageSource().contains("Credenziali non corrette! Riprova.");
+    Assert.assertTrue(isLoginNotOk);
+
+  }
+
+  @Test
+  public void testLoginEntePasswordNotOk(){
+    driver.get("http://localhost:8080/SorrentoMarina/login");
+    WebElement userField = driver.findElement(By.name("username"));
+    userField.sendKeys("root");
+    WebElement passwordField = driver.findElement(By.name("password"));
+    passwordField.sendKeys("456");
+    WebElement form = driver.findElement(By.name("LoginForm"));
+    form.submit();
+    Boolean isLoginNotOk = driver.getPageSource().contains("Credenziali non corrette! Riprova.");
+    Assert.assertTrue(isLoginNotOk);
+
+  }
+
+  @Test
+  public void testLoginEnteEmptyFields(){
+    driver.get("http://localhost:8080/SorrentoMarina/login");
+    WebElement userField = driver.findElement(By.name("username"));
+    userField.sendKeys("");
+    WebElement passwordField = driver.findElement(By.name("password"));
+    passwordField.sendKeys("");
+    WebElement form = driver.findElement(By.name("LoginForm"));
+    form.submit();
+    Boolean isLoginNotOk = driver.getPageSource().contains("Credenziali non corrette! Riprova.");
+    Assert.assertTrue(isLoginNotOk);
+
+  }
+}
